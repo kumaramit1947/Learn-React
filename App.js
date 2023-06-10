0a14e25fe36e161b36765490bac5be28658a690d
Kumar <kumaramitg1947@gmail.com>
2023-06-10 09:40:04 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement('h1', { className: 'react-head' }, 'Jai Shree Ram');
+// Nested sibling elements
+const wrapper = React.createElement('div', { className: 'parent' },
+    [heading, React.createElement('p', { className: 'desc' }, 'Lord of the World')]
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(wrapper);
